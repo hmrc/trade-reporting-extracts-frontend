11532c341f88ca7d5634072b8acc97cd40a7e88a
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2025-01-29 11:31:23 +0000

    Tre 191 (#4)
    * TRE-191: Adding scaffold to project.
    
    * TRE-191: Adding scaffold to project.
    
    * TRE-191: Adding scaffold to project.
    
    * TRE-191: Adding scaffold to project.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,24 +1,22 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.6.0"
-  private val hmrcMongoVersion = "2.4.0"
+  private val bootstrapVersion = "9.4.0"
+  private val hmrcMongoVersion = "2.2.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "11.9.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+    play.sbt.PlayImport.ws,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "10.10.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
-  )
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0"
+  ).map(_ % Test)
 
-  val it = Seq.empty
+  def apply(): Seq[ModuleID] = compile ++ test
 }
