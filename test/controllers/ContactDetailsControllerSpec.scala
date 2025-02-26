c7a519dac959b56150ae19ff6f8ba77adec8a532
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2025-02-26 11:21:48 +0000

    TRE-248: Test case modified.

--- test/controllers/ContactDetailsControllerSpec.scala
+++ test/controllers/ContactDetailsControllerSpec.scala
@@ -2,19 +2,31 @@ package controllers
 
 import base.SpecBase
 import models.{AddressInformation, CompanyInformation}
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.{any, anyString}
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
+import play.api.{Application, inject}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
+import services.TradeReportingExtractsService
+import uk.gov.hmrc.http.{HeaderCarrier, HttpReads}
+import uk.gov.hmrc.http.client.{HttpClientV2, RequestBuilder}
 import views.html.ContactDetailsView
+
+import java.net.URL
+import scala.concurrent.{ExecutionContext, Future}
 
 class ContactDetailsControllerSpec extends SpecBase {
 
   "ContactDetails Controller" - {
 
-    "must return OK and the correct view for a GET" in {
-
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+    "must return OK and the correct view for a GET" in new Setup {
 
       running(application) {
+        when(mockApiService.getCompanyInformation(any[String])(any[HeaderCarrier]))
+          .thenReturn(Future.successful(companyInformation))
+
         val request = FakeRequest(GET, routes.ContactDetailsController.onPageLoad().url)
 
         val result = route(application, request).value
@@ -22,9 +34,21 @@ class ContactDetailsControllerSpec extends SpecBase {
         val view = application.injector.instanceOf[ContactDetailsView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(CompanyInformation("ABC Company", "1",
-          AddressInformation("XYZ Street", "ABC City", Some("G11 2ZZ"), "GB")), "GB123456789002")(request, messages(application)).toString
+        contentAsString(result) mustEqual view(companyInformation, eori)(request, messages(application)).toString
       }
     }
+  }
+
+  trait Setup {
+    val mockApiService: TradeReportingExtractsService = mock[TradeReportingExtractsService]
+    val companyInformation: CompanyInformation = CompanyInformation("ABC Company", "1",
+      AddressInformation("XYZ Street", "ABC City", Some("G11 2ZZ"), "GB"))
+    val eori = "GB123456789002"
+    val application: Application = applicationBuilder()
+      .overrides(
+        inject.bind[TradeReportingExtractsService].toInstance(mockApiService)
+      )
+      .configure("features.new-agent-view-enabled" -> false)
+      .build()
   }
 }
