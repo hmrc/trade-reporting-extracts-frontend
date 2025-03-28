df5e02b395584973ab33eb34aee34159d9991d96
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2025-03-28 14:08:45 +0000

    Formatted files and added license headers

--- test/controllers/report/EoriRoleControllerSpec.scala
+++ test/controllers/report/EoriRoleControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.report
 
 import base.SpecBase
@@ -25,7 +41,7 @@ class EoriRoleControllerSpec extends SpecBase with MockitoSugar {
 
   lazy val eoriRoleRoute: String = controllers.report.routes.EoriRoleController.onPageLoad(NormalMode).url
 
-  val formProvider = new EoriRoleFormProvider()
+  val formProvider              = new EoriRoleFormProvider()
   val form: Form[Set[EoriRole]] = formProvider()
 
   "EoriRole Controller" - {
