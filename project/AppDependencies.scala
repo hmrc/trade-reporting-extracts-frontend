a5deabf21919766156fc3dfc07d733b3c6feb198
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2025-04-01 10:25:59 +0100

    Pr bot suggested version upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,12 +7,12 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.13.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
-  val test                   = Seq(
+  val test = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.17.0"
