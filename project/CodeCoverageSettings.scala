e6e525e51213273227aca9f35c42491d58e7bbc5
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2025-06-02 11:04:01 +0100

    TRE-400: Reduce minimum statement coverage threshold from 80 to 75 in CodeCoverageSettings

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -31,7 +31,7 @@ object CodeCoverageSettings {
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
     ScoverageKeys.coverageExcludedFiles := excludedFiles.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80,
+    ScoverageKeys.coverageMinimumStmtTotal := 75,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
