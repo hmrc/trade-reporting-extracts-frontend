4fc9d114154a5e46eae53ca4120405afdcc38974
MichaelAAbbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-07-16 16:14:37 +0100

    Resolved conflicts

--- app/controllers/report/RequestConfirmationController.scala
+++ app/controllers/report/RequestConfirmationController.scala
@@ -58,7 +58,8 @@ class RequestConfirmationController @Inject() (
     val isMoreThanOneReport      = ReportHelpers.isMoreThanOneReport(request.userAnswers)
 
     for {
-      notificationEmail <- tradeReportingExtractsService.getNotificationEmail(request.eori)requestRefs                      <- tradeReportingExtractsService.createReportRequest(
+      notificationEmail <- tradeReportingExtractsService.getNotificationEmail(request.eori)
+      requestRefs                      <- tradeReportingExtractsService.createReportRequest(
                                             reportRequestDataService.buildReportRequest(request.userAnswers, request.eori)
                                           )
       requestRef                        = requestRefs.mkString(", ")
