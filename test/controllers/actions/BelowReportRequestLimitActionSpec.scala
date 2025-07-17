aba125c1c54fe8dc7e462842fa2db068428515da
ElliotAndersonHMRC <114657325+ElliotAndersonHMRC@users.noreply.github.com>
2025-07-17 13:03:49 +0100

    TRE-644 small fixes

--- test/controllers/actions/BelowReportRequestLimitActionSpec.scala
+++ test/controllers/actions/BelowReportRequestLimitActionSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.actions
 
 import base.SpecBase
@@ -19,12 +35,12 @@ import scala.concurrent.{ExecutionContext, Future}
 class BelowReportRequestLimitActionSpec extends SpecBase with MockitoSugar with ScalaFutures {
 
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-  implicit val hc: HeaderCarrier = HeaderCarrier()
+  implicit val hc: HeaderCarrier    = HeaderCarrier()
 
   val mockTradeReportingExtractsService: TradeReportingExtractsService = mock[TradeReportingExtractsService]
 
   class Harness(service: TradeReportingExtractsService)(implicit ec: ExecutionContext)
-    extends BelowReportRequestLimitActionImpl(service) {
+      extends BelowReportRequestLimitActionImpl(service) {
     def callRefine[A](request: DataRequest[A]): Future[Either[Result, DataRequest[A]]] = refine(request)
   }
 
@@ -60,7 +76,8 @@ class BelowReportRequestLimitActionSpec extends SpecBase with MockitoSugar with 
       result mustBe a[Left[_, _]]
       val redirect = result.left.get
       redirect.header.status mustBe SEE_OTHER
-      redirect.header.headers("Location") mustBe controllers.problem.routes.TooManySubmissionsController.onPageLoad().url
+      redirect.header
+        .headers("Location") mustBe controllers.problem.routes.TooManySubmissionsController.onPageLoad().url
     }
   }
 }
