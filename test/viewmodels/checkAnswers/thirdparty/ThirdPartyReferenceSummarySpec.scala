2c80052a001ff314ffa2515f2e6f1c6f186df2fa
MichaelAAbbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-08-13 15:55:29 +0100

    TRE-593 Allow users to give a third party a reference name

---
+++ test/viewmodels/checkAnswers/thirdparty/ThirdPartyReferenceSummarySpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.thirdparty
+
+import base.SpecBase
+import models.{CheckMode, UserAnswers}
+import pages.thirdparty.ThirdPartyReferencePage
+import play.api.i18n.Messages
+import play.api.test.Helpers.stubMessages
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Actions, SummaryListRow}
+import viewmodels.govuk.summarylist.*
+import viewmodels.implicits.*
+
+class ThirdPartyReferenceSummarySpec extends SpecBase {
+
+  implicit private val messages: Messages = stubMessages()
+
+  "ThirdPartyReferenceSummarySpec.row" - {
+
+    "must return a SummaryListRow when an answer is present" in {
+      val answer      = "thirdPartyRef"
+      val userAnswers = UserAnswers("id").set(ThirdPartyReferencePage, answer).success.value
+
+      val result = ThirdPartyReferenceSummary.row(userAnswers)
+
+      result mustBe Some(
+        SummaryListRow(
+          key = "thirdPartyReference.checkYourAnswersLabel",
+          value = ValueViewModel(Text(HtmlFormat.escape(answer).toString)),
+          actions = Some(
+            Actions(items =
+              Seq(
+                ActionItemViewModel(
+                  "site.change",
+                  controllers.thirdparty.routes.ThirdPartyReferenceController.onPageLoad(CheckMode).url
+                ).withVisuallyHiddenText(messages("thirdPartyReference.change.hidden"))
+              )
+            )
+          )
+        )
+      )
+    }
+  }
+
+  "must return None when no answer is present" in {
+    val userAnswers = UserAnswers("id")
+
+    val result = ThirdPartyReferenceSummary.row(userAnswers)
+
+    result mustBe None
+  }
+}
