794adea3bd8a1aed104aa8db0a6597d618829aab
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2025-08-21 09:32:21 +0100

    TRE-585 correcting mongo path and introducing nav fragments

--- app/pages/thirdparty/EoriNumberPage.scala
+++ app/pages/thirdparty/EoriNumberPage.scala
@@ -21,7 +21,7 @@ import play.api.libs.json.JsPath
 
 case object EoriNumberPage extends QuestionPage[String] {
 
-  override def path: JsPath = JsPath \ toString
+  override def path: JsPath = JsPath \ "addThirdParty" \ toString
 
   override def toString: String = "eoriNumber"
 }
