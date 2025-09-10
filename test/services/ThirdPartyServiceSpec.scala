364e4d448fff01eb564e4046e88bebc790ae1a0a
sukesh90 <35919670+sukesh90@users.noreply.github.com>
2025-09-10 15:46:22 +0100

    changes for spec file

--- test/services/ThirdPartyServiceSpec.scala
+++ test/services/ThirdPartyServiceSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package services
 
 import org.scalatestplus.mockito.MockitoSugar
@@ -5,7 +21,7 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.TryValues._
-import models.thirdparty.{ThirdPartyRequest, DeclarationDate, DataTypes}
+import models.thirdparty.{DataTypes, DeclarationDate, ThirdPartyRequest}
 import models.UserAnswers
 import pages.thirdparty._
 import java.time.{Clock, Instant, LocalDate, ZoneId}
@@ -13,8 +29,8 @@ import java.time.{Clock, Instant, LocalDate, ZoneId}
 class ThirdPartyServiceSpec extends AnyWordSpec with Matchers with MockitoSugar with ScalaFutures {
 
   val fixedInstant: Instant = Instant.parse("2024-01-01T00:00:00Z")
-  val fixedClock: Clock = Clock.fixed(fixedInstant, ZoneId.of("UTC"))
-  val service = new ThirdPartyService(fixedClock)
+  val fixedClock: Clock     = Clock.fixed(fixedInstant, ZoneId.of("UTC"))
+  val service               = new ThirdPartyService(fixedClock)
 
   "ThirdPartyService" should {
 
@@ -22,10 +38,18 @@ class ThirdPartyServiceSpec extends AnyWordSpec with Matchers with MockitoSugar 
 
       "populate all fields correctly when all data is present" in {
         val userAnswers = UserAnswers("id")
-          .set(EoriNumberPage, "GB2").success.value
-          .set(ThirdPartyAccessStartDatePage, LocalDate.of(2024, 1, 1)).success.value
-          .set(DataTypesPage, Set(DataTypes.Import)).success.value
-          .set(ThirdPartyReferencePage, "ref").success.value
+          .set(EoriNumberPage, "GB2")
+          .success
+          .value
+          .set(ThirdPartyAccessStartDatePage, LocalDate.of(2024, 1, 1))
+          .success
+          .value
+          .set(DataTypesPage, Set(DataTypes.Import))
+          .success
+          .value
+          .set(ThirdPartyReferencePage, "ref")
+          .success
+          .value
 
         val result = service.buildThirdPartyAddRequest(userAnswers, "GB1")
 
@@ -39,7 +63,9 @@ class ThirdPartyServiceSpec extends AnyWordSpec with Matchers with MockitoSugar 
 
       "set reportDateStart and reportDateEnd to None for AllAvailableData" in {
         val userAnswers = UserAnswers("id")
-          .set(DeclarationDatePage, DeclarationDate.AllAvailableData).success.value
+          .set(DeclarationDatePage, DeclarationDate.AllAvailableData)
+          .success
+          .value
 
         val result = service.buildThirdPartyAddRequest(userAnswers, "GB1")
         result.reportDateStart mustBe None
@@ -47,12 +73,16 @@ class ThirdPartyServiceSpec extends AnyWordSpec with Matchers with MockitoSugar 
       }
 
       "set reportDateStart and reportDateEnd for CustomDateRange" in {
-        val customDate = LocalDate.of(2023, 12, 31)
+        val customDate  = LocalDate.of(2023, 12, 31)
         val userAnswers = UserAnswers("id")
-          .set(DeclarationDatePage, DeclarationDate.CustomDateRange).success.value
-          .set(DataStartDatePage, customDate).success.value
+          .set(DeclarationDatePage, DeclarationDate.CustomDateRange)
+          .success
+          .value
+          .set(DataStartDatePage, customDate)
+          .success
+          .value
 
-        val result = service.buildThirdPartyAddRequest(userAnswers, "GB1")
+        val result          = service.buildThirdPartyAddRequest(userAnswers, "GB1")
         val expectedInstant = customDate.atStartOfDay(fixedClock.getZone).toInstant
         result.reportDateStart mustBe Some(expectedInstant)
         result.reportDateEnd mustBe Some(expectedInstant)
@@ -60,14 +90,16 @@ class ThirdPartyServiceSpec extends AnyWordSpec with Matchers with MockitoSugar 
 
       "use current instant for accessStart if not present" in {
         val userAnswers = UserAnswers("id")
-        val result = service.buildThirdPartyAddRequest(userAnswers, "GB1")
+        val result      = service.buildThirdPartyAddRequest(userAnswers, "GB1")
         result.accessStart mustBe fixedInstant
       }
 
       "set accessEnd if present" in {
-        val endDate = LocalDate.of(2024, 2, 1)
+        val endDate     = LocalDate.of(2024, 2, 1)
         val userAnswers = UserAnswers("id")
-          .set(ThirdPartyAccessEndDatePage, Some(endDate)).success.value
+          .set(ThirdPartyAccessEndDatePage, Some(endDate))
+          .success
+          .value
 
         val result = service.buildThirdPartyAddRequest(userAnswers, "GB1")
         result.accessEnd mustBe Some(endDate.atStartOfDay(fixedClock.getZone).toInstant)
